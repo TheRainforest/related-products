67a54459c00f9ac2669c379e230ea5fde5c537d9
Ubuntu <ubuntu@ip-172-31-89-156.ec2.internal>
2020-06-03 20:29:10 +0000

    Adapt in deployment

--- server/database/index.js
+++ server/database/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const postgres = require('postgres');
 
 const db = postgres({
-  max: 10,
+  max: 100,
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   transform: {
     value(item) {
